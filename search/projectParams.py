b4d40e98dd54862b86f6243f4814f87751a9e3c3
ffalathel <fahdahalathel@gmail.com>
2026-01-04 22:58:00 -0500

    adding
    added project 1

---
+++ search/projectParams.py
@@ -0,0 +1,22 @@
+# projectParams.py
+# ----------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+# Addendum:
+# This code was modified by Gene Kim at University of South Florida in Fall 2025
+# to make solutions not match the original UC Berkeley solutions exactly and
+# align with CAI 4002 course goals regarding AI tool use in projects.
+
+STUDENT_CODE_DEFAULT = 'searchAgents.py,search.py'
+PROJECT_TEST_CLASSES = 'searchTestClasses.py'
+PROJECT_NAME = 'Project 1: Search'
+BONUS_PIC = False
